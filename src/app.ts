539a860e1687a02bf9c372c286da828899a9331e
Preyansh Mitharwal <preyansh.mitharwal@gmail.com>
2020-12-21 13:26:20 +0530

    feat: add CORS headers to API response

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 import { config } from "./config/app-config";
 import { router as healthRouter } from "./routes/health";
 import { router as swaggerRouter } from "./routes/swagger";
 import { router as reposRouter } from "./routes/repos";
 
 const app = express();
+
+app.use(cors());
 
 app.use(healthRouter);
 app.use(swaggerRouter);
